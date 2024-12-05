fe664d9b8489e98ab763f38da6733bd11247786d
cassiaqueiroz <cassiarsqueiroz@gmail.com>
2024-12-05 00:27:45 -0300

    change cirurgias section style

--- assets/js/main.js
+++ assets/js/main.js
@@ -144,3 +144,13 @@
   document.addEventListener('scroll', navmenuScrollspy);
 
 })();
+
+  /**
+   * Frequently Asked Questions Toggle
+   */
+  document.querySelectorAll('.faq-item h3, .faq-item .faq-toggle').forEach((faqItem) => {
+    faqItem.addEventListener('click', () => {
+      faqItem.parentNode.classList.toggle('faq-active');
+    });
+  });
+  
